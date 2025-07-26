f0a02c3c45e938f8b5a5aa40890f5ca28c23ef0a
betabites <61530254+betabites@users.noreply.github.com>
2025-07-26 17:04:52 +1200

    Added "/activity" subcommand for Pterodactyl module, integrated session tracking and BigQuery, refactored point system for Valheim module, and introduced Minecraft module.

--- server_src/bot/modules/Valheim.ts
+++ server_src/bot/modules/Valheim.ts
@@ -19,20 +19,6 @@ const topicName = 'pterodactyl-connection-history-measure '
 const subscriptionName = 'crashbot-connection-history-subscription'
 
 export class Valheim extends BaseModule {
-    commands = [
-        new SlashCommandBuilder()
-            .setName("valheim")
-            .setDescription("View Valheim server information")
-            .addSubcommand(subcommand => subcommand
-                .setName("activity")
-                .setDescription("View the server's activity for the last 30 days")
-                .addStringOption(s => s.setName("username").setDescription("The username of the player to view").setRequired(false))
-                .addBooleanOption(s => s.setName("show_shares").setDescription("Shows how the user's stats compare against others").setRequired(false))
-                .addBooleanOption(s => s.setName("last_month").setDescription("Show details for last month instead").setRequired(false))
-            )
-            .setDefaultMemberPermissions(null)
-    ]
-
     constructor(client: Client) {
         super(client);
         this.#configurePubSubListener()
@@ -40,16 +26,15 @@ export class Valheim extends BaseModule {
 
         setInterval(async () => {
             // Award 1 point to all connected users every 20 minutes
-            let discord_ids = await SafeQuery<{ discord_id: string }>(sql`
-                SELECT users.discord_id AS 'discord_id'
+            let discord_ids = await contextSQL<{ user_id: string }>`
+                SELECT user_id
                 FROM dbo.ValheimConnectionHistory history
-                JOIN dbo.Users users ON history.username = users.valheim_name
                 WHERE history.sessionEnd IS NULL
-            `)
+            `
             for (let user of discord_ids.recordset) {
                 await grantPointsWithDMResponse({
                     discordClient: client,
-                    user: new User(user.discord_id),
+                    user: new User(user.user_id),
                     reason: "Valheim",
                     points: 1,
                     capped: true,
@@ -58,18 +43,17 @@ export class Valheim extends BaseModule {
         }, 1.2e+6)
         setInterval(async () => {
             // Award points every 5 minutes if 3+ players are connected
-            let discord_ids = await SafeQuery<{ discord_id: string }>(sql`
-                SELECT users.discord_id AS 'discord_id'
+            let discord_ids = await contextSQL<{ user_id: string }>`
+                SELECT user_id
                 FROM dbo.ValheimConnectionHistory history
-                JOIN dbo.Users users ON history.username = users.valheim_name
                 WHERE history.sessionEnd IS NULL
-            `)
+            `
             if (discord_ids.recordset.length < 3) return
 
             for (let user of discord_ids.recordset) {
                 await grantPointsWithDMResponse({
                     discordClient: client,
-                    user: new User(user.discord_id),
+                    user: new User(user.user_id),
                     reason: "Valheim (group)",
                     points: discord_ids.recordset.length - 2,
                     capped: true,
@@ -240,275 +224,39 @@ export class Valheim extends BaseModule {
         console.log("SESSION RESULTS", sessions.filter(s => s.start.getDate() === 17))
     }
 
-    async calculateHistoricalPoints() {
-        let usersSearch = await SafeQuery<{
-            discord_id: string,
-            valheim_name: string
-        }>(sql`SELECT discord_id, valheim_name
-               FROM dbo.Users`)
-        let users = new Map<string, { discord_id: string, points: number }>()
-        for (let user of usersSearch.recordset) {
-            if (!user.valheim_name) continue
-            users.set(user.valheim_name, {discord_id: user.discord_id, points: 0 })
-        }
-
-        let history = await SafeQuery<{ username: string, sessionStart?: Date, sessionEnd?: Date }>(
-            sql`SELECT username, sessionStart, sessionEnd
-                FROM dbo.ValheimConnectionHistory
-                ORDER BY sessionStart`
-        )
-        let switches: { username: string, isEnd: boolean, date: Date }[] = []
-        // Calculate switches
-        for (let item of history.recordset) {
-            if (!item.sessionStart || !item.sessionEnd) continue
-            switches.push({
-                username: item.username,
-                isEnd: false,
-                date: item.sessionStart
-            })
-            switches.push({
-                username: item.username,
-                isEnd: true,
-                date: item.sessionEnd
-            })
-        }
-        // Sort switches
-        switches = switches.sort((a, b) => a.date.getTime() - b.date.getTime())
-
-        // Calculate points
-        let lastDate = switches[0].date
-        let activeUsers = new Set<string>()
-        for (let switchItem of switches) {
-            // Award points to active users
-            if (activeUsers.size >= 3) {
-                for (let activeUser of activeUsers) {
-                    let user = users.get(activeUser)
-                    if (!user) continue
-                    console.log("MULTIPLYING", activeUsers.size - 2)
-                    let multiplier = activeUsers.size >= 3 ? (activeUsers.size - 2) : .25
-                    user.points += Math.floor(Math.floor((switchItem.date.getTime() - lastDate.getTime()) / 300000) * multiplier)
-                    users.set(activeUser, user)
-                }
-            }
-            if (switchItem.isEnd) activeUsers.delete(switchItem.username)
-            else activeUsers.add(switchItem.username)
-
-            lastDate = switchItem.date
-        }
-
-        // grant points
-        // await Promise.allSettled([...users].map(async ([username, user]) => {
-        //     if (!user.points || user.points <= 0) return
-        //     await PointsModule.grantPointsWithDMResponse({
-        //         discordClient: this.client,
-        //         points: user.points,
-        //         reason: "Valheim (retroactive)",
-        //         userDiscordId: user.discord_id,
-        //     })
-        // }))
-        console.log("USERS", JSON.stringify([...users]))
-    }
-
-    @InteractionChatCommandResponse("valheim")
-    async onValheimCommand(interaction: ChatInputCommandInteraction) {
-        switch (interaction.options.getSubcommand()) {
-            case "activity":
-                await interaction.deferReply({ephemeral: true})
-                // Get billing data
-                const bigquery = new BigQuery();
-                const [rows] = await bigquery.query(`SELECT DATE(usage_end_time, "UTC") as usage_date,
-                                                            SUM(cost)                   as cost
-                                                     FROM \`re-flesh.usage_costs.gcp_billing_export_v1_0176BB_97AC5E_923E49\`
-                                                     GROUP BY DATE (usage_end_time, "UTC")`);
-
-                // Get session data
-                let userData = await SafeQuery<{ valheim_name: string }>(sql`SELECT valheim_name
-                                                                             FROM dbo.Users
-                                                                             WHERE discord_id = ${interaction.user.id}`)
-                if (!userData.recordset[0]?.valheim_name) {
-                    void interaction.editReply({content: "It appears this command hasn't been correctly configured for you yet. Please contact @Beta."})
-                    return
-                }
-                let username = userData.recordset[0]?.valheim_name
-                if (interaction.options.getString("username")) {
-                    if (!["393955339550064641", "404507305510699019"].includes(interaction.user.id)) {
-                        interaction.editReply({content: "You aren't authorised to view the activity of other players"})
-                        return
-                    }
-                    username = interaction.options.getString("username", true)
-                }
-                let showShares = false
-                if (interaction.options.getBoolean("show_shares", false) !== null) {
-                    // if (!["393955339550064641", "404507305510699019"].includes(interaction.user.id)) {
-                    //     interaction.editReply({content: "You aren't authorised to view the shared activity of other players"})
-                    //     return
-                    // }
-                    showShares = true
-                }
-
-                let beginningOfMonth = new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth(), 1))
-                let beginningOfNextMonth = new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth() + 1, 1))
-                if (!!interaction.options.getBoolean("last_month", false)) {
-                    beginningOfMonth = new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth() - 1, 1))
-                    beginningOfNextMonth = new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth(), 1))
-                }
-                let currentMonthHistory = await SafeQuery<{ sessionStart: Date, sessionEnd: Date, username: string }>(
-                    sql`SELECT sessionEnd,
-                               username,
-                               sessionStart
-                        FROM dbo.ValheimConnectionHistory
-                        WHERE sessionEnd IS NOT NULL
-                          AND sessionStart IS NOT NULL
-                          AND sessionEnd >= ${beginningOfMonth}
-                          AND sessionEnd < ${beginningOfNextMonth}
-                          AND username = ${username}`
-                )
-                let totalHistoryData = await SafeQuery<{ sessionEnd: Date, sessionStart: Date, username: string }>(
-                    sql`SELECT sessionEnd,
-                               username,
-                               sessionStart
-                        FROM dbo.ValheimConnectionHistory
-                        WHERE sessionEnd IS NOT NULL
-                          AND sessionStart IS NOT NULL
-                          AND sessionEnd >= ${beginningOfMonth}
-                          AND sessionEnd < ${beginningOfNextMonth}
-                `)
-                let sessionPlaytimeAverage = await SafeQuery<{ median: number }>(sql`WITH UserMedians AS (
-                    SELECT
-                        username,
-                        PERCENTILE_CONT(0.5)
-                                        WITHIN GROUP (ORDER BY DATEDIFF(millisecond, sessionStart, sessionEnd))
-                                        OVER (PARTITION BY username) as median
-                    FROM ValheimConnectionHistory
-                    WHERE sessionEnd IS NOT NULL
-                )
-                                                                                      SELECT DISTINCT username, median
-                                                                                      FROM UserMedians
-                                                                                      WHERE username = ${username}`)
-
-                let embeds: EmbedBuilder[] = []
-
-                let totalSpend = 0
-
-                let usernameToDateAndDuration = sessionsToDurationMap(totalHistoryData.recordset, beginningOfMonth)
-                let dateToDuration = usernameToDateAndDuration.get(username) ?? new Map()
-                let sessionEmbeds: {date: Date, embed: EmbedBuilder}[] = []
-
-                for (let [dateString, sessionDuration] of dateToDuration) {
-
-                    let totalForThisDay = 0
-                    let shares = new Map<string, number>()
-                    for (let [username, dateToDuration] of usernameToDateAndDuration) {
-                        let duration = dateToDuration.get(dateString)
-                        if (!duration) continue
-                        totalForThisDay += duration
-                        shares.set(username, duration)
-                    }
-                    if (!totalForThisDay) throw new Error("No total for this day")
-                    let percentage = sessionDuration / totalForThisDay
-                    let todaysSpendature = rows.find(x =>
-                        (x.usage_date as BigQueryDate).value === dateString
-                    )
-                    if (todaysSpendature) totalSpend += todaysSpendature.cost * percentage
-
-                    // Only include embeds for the last 6 sessions/days
-                    if (showShares) {
-                        if (!todaysSpendature) {
-                            sessionEmbeds.push({
-                                date: new Date(dateString),
-                                embed: new EmbedBuilder()
-                                    .setDescription(
-                                        `\`${dateString}\` - You were online for \`${toTimeDifferenceString(sessionDuration)}\`\n` +
-                                        [...shares].map(([username, duration]) => `- \`${toTimeDifferenceString(duration)}\` - ${username} - ${Math.round((duration / totalForThisDay) * 100)}%`).join(`\n`)
-                                    )
-                            })
-                            continue;
-                        }
-
-                        sessionEmbeds.push({
-                            date: new Date(dateString),
-                            embed: new EmbedBuilder()
-                                .setDescription(`\`${dateString}\` - You were online for \`${toTimeDifferenceString(sessionDuration)}\` - $${
-                                    Math.round((todaysSpendature.cost * percentage) * 100) / 100
-                                } (%${Math.round(percentage * 100)} of $${Math.round(todaysSpendature.cost * 100) / 100})\n` +
-                                    [...shares].map(([username, duration]) => `- \`${toTimeDifferenceString(duration)}\` - ${username} - ${Math.round((duration / totalForThisDay) * 100)}%`).join(`\n`))
-
-                        })
-                    } else {
-                        if (!todaysSpendature) {
-                            sessionEmbeds.push({
-                                date: new Date(dateString),
-                                embed: new EmbedBuilder()
-                                    .setDescription(`\`${dateString}\` - You were online for \`${toTimeDifferenceString(sessionDuration)}\``)
-                            })
-                            continue;
-                        }
-
-                        sessionEmbeds.push({
-                            date: new Date(dateString),
-                            embed: new EmbedBuilder()
-                                .setDescription(`\`${dateString}\` - You were online for \`${toTimeDifferenceString(sessionDuration)}\` - $${
-                                    Math.round((todaysSpendature.cost * percentage) * 100) / 100
-                                } (%${Math.round(percentage * 100)} of $${Math.round(todaysSpendature.cost * 100) / 100})`)
-
-                        })
-                    }
-                }
-                sessionEmbeds = sessionEmbeds.sort((a, b) => b.date.getTime() - a.date.getTime())
-                if (sessionEmbeds.length > 6) sessionEmbeds = sessionEmbeds.splice(0, 6)
-                embeds.push(...sessionEmbeds.map(i => i.embed))
-                embeds.unshift(
-                    new EmbedBuilder()
-                        .setThumbnail("https://wallpapers.com/images/hd/valheim-stunning-black-forest-cbbzj0pqyrz4pyoa.jpg")
-                        .setTitle("Valheim Activity")
-                        .setDescription(currentMonthHistory.recordset.length === 0
-                            ? "No activity recorded for this month."
-                            : `This month, you have logged in ${currentMonthHistory.recordset.length} times.
-Total time logged in: \`${toTimeDifferenceString(
-                                currentMonthHistory.recordset.reduce((acc, x) => acc + x.sessionEnd.getTime() - x.sessionStart.getTime(), 0)
-                            )}\`. $${Math.round(totalSpend * 100) / 100} NZD
-Your current average (median) is \`${toTimeDifferenceString(sessionPlaytimeAverage.recordset[0]?.median ?? 0)}\` per session.`)
-                        .setFooter({
-                            text: "Dates and times shown may be in UTC"
-                        })
-                )
-
-                void interaction.editReply({embeds})
-        }
-    }
-
     // Runs when a new connection is made to the server
+    static async getUserFromValheimName(valheim_name: string): Promise<User | null> {
+        const user_search = await contextSQL<{discord_id: string}>`SELECT id FROM Users WHERE valheim_name=${valheim_name}`
+        const user = user_search.recordset[0]
+        return user ? new User(user.discord_id) : null
+    }
     async #submitNewConnection(timestamp: Date, zdoId: string, username: string) {
         // console.log("STARTING SESSION", zdoId, " ", timestamp, "")
+        // Get the user
+        const user = await Valheim.getUserFromValheimName(username)
 
         // First, check to see if a connection is already active
-        let res = await SafeQuery(
-            sql`UPDATE dbo.ValheimConnectionHistory SET sessionStart = ${timestamp}, username = ${username} WHERE ZDO_ID = ${zdoId} AND sessionStart IS NULL`
-        )
+        let res = await contextSQL`UPDATE dbo.ValheimConnectionHistory SET sessionStart = ${timestamp}, user_id = ${user?.discord_id ?? null} WHERE ZDO_ID = ${zdoId} AND sessionStart IS NULL`
         if (!!res.rowsAffected[0]) return
         res = await contextSQL`SELECT * FROM dbo.ValheimConnectionHistory WHERE ZDO_ID = ${zdoId} AND sessionStart IS NOT NULL AND sessionEnd IS NULL`
         if (!!res.recordset[0]) return
 
-        await SafeQuery(sql`INSERT INTO dbo.ValheimConnectionHistory (id, ZDO_ID, sessionStart, username)
-                            VALUES (NEWID(), ${zdoId}, ${timestamp}, ${username})`)
+        await contextSQL`INSERT INTO dbo.ValheimConnectionHistory (id, ZDO_ID, sessionStart, user_id)
+                            VALUES (NEWID(), ${zdoId}, ${timestamp}, ${user?.discord_id ?? null})`
     }
 
     async #submitNewDisconnection(timestamp: Date, zdoId: string) {
         // First, check to see if a connection is already active
         // console.log("ENDING SESSION", zdoId, " ", timestamp, "")
-        let res = await SafeQuery(
-            sql`SELECT sessionEnd, id FROM dbo.ValheimConnectionHistory WHERE ZDO_ID = ${zdoId}`
-        )
+        let res = await contextSQL<{ sessionEnd: Date, id: string }>`SELECT sessionEnd, id FROM dbo.ValheimConnectionHistory WHERE ZDO_ID = ${zdoId}`
         if (!res.recordset[0]) {
-            await SafeQuery(sql`INSERT INTO dbo.ValheimConnectionHistory (id, ZDO_ID, sessionEnd)
-                                VALUES (NEWID(), ${zdoId}, ${timestamp})`)
+            await contextSQL`INSERT INTO dbo.ValheimConnectionHistory (id, ZDO_ID, sessionEnd)
+                                VALUES (NEWID(), ${zdoId}, ${timestamp})`
             return
         }
 
         let currentSession = res.recordset.find(x => x.sessionEnd === null)
-        if (currentSession) await SafeQuery(
-            sql`UPDATE dbo.ValheimConnectionHistory SET sessionEnd = ${timestamp} WHERE id = ${currentSession.id}`
-        )
+        if (currentSession) await contextSQL`UPDATE dbo.ValheimConnectionHistory SET sessionEnd = ${timestamp} WHERE id = ${currentSession.id}`
     }
 
     async #closeAllOpenConnections(timestamp: Date) {
