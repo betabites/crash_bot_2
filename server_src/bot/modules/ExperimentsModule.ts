81e4213eaf9ac827f3e85cfe1960b24894d5178d
betabites <61530254+betabites@users.noreply.github.com>
2025-11-11 22:34:40 +1300

    Adjusted I alone am

--- server_src/bot/modules/ExperimentsModule.ts
+++ server_src/bot/modules/ExperimentsModule.ts
@@ -59,6 +59,9 @@ export class ExperimentsModule extends BaseModule {
         if (!iAmNotTest) iAmNotTest = /i'm not([^.\n!?]*)/g.exec(msg.content.toLowerCase())
         let iAmTest = /i am([^.\n!?]*)/g.exec(msg.content.toLowerCase())
         if (!iAmTest) iAmTest = /i'm([^.\n!?]*)/g.exec(msg.content.toLowerCase())
+
+        let iAloneAmTest = /i alone am([^.\n!?]*)/g.exec(msg.content.toLowerCase())
+        let iAloneAmNotTest = /i alone am not([^.\n!?]*)/g.exec(msg.content.toLowerCase())
         if (msg.author.bot || msg.channel.isDMBased() || !msg.channel.isSendable()) return
         if (msg.content.toLowerCase().includes("how many times have i said ")) {
             getUserData(msg.member as GuildMember)
@@ -246,10 +249,18 @@ export class ExperimentsModule extends BaseModule {
             msg.member.setNickname(iAmTest[1].toLowerCase().trim().substring(0, 32))
             msg.reply(`Hi <@${msg.member?.id}>!`)
         }
-        else if (msg.content.toLowerCase().startsWith("i alone am")) {
+        else if (iAloneAmNotTest) {
+            if (!msg.member || !msg.guild) return
+            const res = await openai.sendMessage(`opposite of '${iAloneAmNotTest[1]}'. Only say the answer. nothing else. Be funny and short.`)
+            msg.guild.members.fetch(this.client.user?.id ?? "").then(me =>
+                me.setNickname(res.text.trim().substring(0, 32))
+            )
+            msg.reply(`not fax`)
+        }
+        else if (iAloneAmTest) {
             if (!msg.member || !msg.guild) return
             msg.guild.members.fetch(this.client.user?.id ?? "").then(me =>
-                me.setNickname(msg.content.toLowerCase().replace("i alone am", "").trim().substring(0, 32))
+                me.setNickname(iAloneAmTest[1].trim().substring(0, 32))
             )
             msg.reply("fax")
         }
